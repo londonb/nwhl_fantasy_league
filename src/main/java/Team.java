61b2ca65834bcda830f3e35451c1425628b8f9c2
abby rolling and tyler suderman <tcsuder@gmail.com>
2016-03-07 11:53:31 -0800

    Start team object

---
+++ src/main/java/Team.java
@@ -0,0 +1,95 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class Team {
+  private int id;
+  private String team_name;
+  private final int MAX_PLAYERS;
+  private int current_players;
+
+
+  public Team(String name) {
+    team_name = name;
+    MAX_PLAYERS = 8;
+    current_players = 0;
+  }
+
+  public String getName() {
+    return team_name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getCurrentPlayers() {
+    return current_players;
+  }
+
+  @Override
+  public boolean equals(Object otherTeam) {
+    if(!(otherTeam instanceof Team)) {
+      return false;
+    } else {
+      Team newTeam = (Team) otherTeam;
+      return this.getId() == newTeam.getId() &&
+        this.getCurrentPlayers() == newTeam.getCurrentPlayers() &&
+        this.getName().equals(newTeam.getName());
+    }
+  }
+
+  //CREATE
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO teams (team_name, current_players) VALUES (:name, :current_players)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", team_name)
+        .addParameter("current_players", current_players)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  //READ
+  public static List<Team> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM teams";
+      return con.createQuery(sql)
+        .executeAndFetch(Team.class);
+    }
+  }
+
+  public static Team find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM teams WHERE id=:id";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Team.class);
+    }
+  }
+  //UPDATE
+
+  public void updateName(String name) {
+    team_name=name;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE teams SET team_name=:team_name WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("team_name", team_name)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  //DELETE
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM teams WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  } // add deletion from join tables AND LEAGUES here!!!
+
+
+}
---
+++ src/test/java/TeamTest.java
@@ -0,0 +1,62 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TeamTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void team_instantiatesCorrectly_true() {
+    Team newTeam = new Team("Cheryl");
+    assertTrue(newTeam instanceof Team);
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Team.all().size(), 0);
+  }
+
+  // @Test
+  // public void equals_returnsTrueIfNamesAreTheSame() {
+  //   Team newTeam = new Team("Cheryl");
+  //   Team newTeam2 = new Team("Cheryl");
+  //   assertTrue(newTeam.equals(newTeam2));
+  // }
+  //
+  // @Test
+  // public void save_savesObjectIntoDBWithID_true() {
+  //   Team newTeam = new Team("Cheryl");
+  //   newTeam.save();
+  //   Team savedTeam = Team.all().get(0);
+  //   assertEquals(savedTeam, newTeam);
+  // }
+  //
+  // @Test
+  // public void find_returnsTeamObjectFromId_true() {
+  //   Team newTeam = new Team("Cheryl");
+  //   newTeam.save();
+  //   Team otherTeam = Team.find(newTeam.getId());
+  //   assertEquals(newTeam, otherTeam);
+  // }
+  //
+  // @Test
+  // public void update_updatesAllTeamsVariables_true() {
+  //   Team newTeam = new Team("Masters of Luck");
+  //   newTeam.save();
+  //   newTeam.updateName("Cheryl");
+  //   Team otherTeam = Team.find(newTeam.getId());
+  //   assertEquals(newTeam, otherTeam);
+  // }
+  //
+  // @Test
+  // public void delete_deletesASpecificLeague() {
+  //   Team newTeam = new Team("Cheryl");
+  //   Team newTeam2 = new Team("Beryl");
+  //   newTeam.save();
+  //   newTeam2.save();
+  //   newTeam.delete();
+  //   assertFalse(Team.all().contains(newTeam));
+  // }
+
+}
